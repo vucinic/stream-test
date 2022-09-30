535815fb3e0d22014c90ddf3e3060bf2dac8154e
vucinic <sobiet@live.it>
2022-09-30 14:07:11 +0200

    delombok

--- src/main/java/Department.java
+++ src/main/java/Department.java
@@ -1,16 +1,76 @@
-import lombok.Data;
-
-import java.util.ArrayList;
 import java.util.List;
 
 
-@Data
 public class Department {
 
     private String name;
     private List<Employee> employees;
     private Double annualBudget;
 
+    public Department() {
+    }
 
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return this.employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Double getAnnualBudget() {
+        return this.annualBudget;
+    }
+
+    public void setAnnualBudget(Double annualBudget) {
+        this.annualBudget = annualBudget;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Department)) return false;
+        final Department other = (Department) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        final Object this$employees = this.getEmployees();
+        final Object other$employees = other.getEmployees();
+        if (this$employees == null ? other$employees != null : !this$employees.equals(other$employees)) return false;
+        final Object this$annualBudget = this.getAnnualBudget();
+        final Object other$annualBudget = other.getAnnualBudget();
+        if (this$annualBudget == null ? other$annualBudget != null : !this$annualBudget.equals(other$annualBudget))
+            return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Department;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final Object $employees = this.getEmployees();
+        result = result * PRIME + ($employees == null ? 43 : $employees.hashCode());
+        final Object $annualBudget = this.getAnnualBudget();
+        result = result * PRIME + ($annualBudget == null ? 43 : $annualBudget.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Department(name=" + this.getName() + ", employees=" + this.getEmployees() + ", annualBudget=" + this.getAnnualBudget() + ")";
+    }
 }
 
--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,16 +1,115 @@
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NonNull;
-
-@Data
 public class Employee {
 
-    @NonNull
     private String surname, name;
     private String address, title;
-    @NonNull
     private Double salary;
-    @NonNull
+
     private Role role;
 
+    public Employee(String surname, String name, Double salary, Role role) {
+        this.surname = surname;
+        this.name = name;
+        this.salary = salary;
+        this.role = role;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Role getRole() {
+        return this.role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Employee)) return false;
+        final Employee other = (Employee) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$surname = this.getSurname();
+        final Object other$surname = other.getSurname();
+        if (this$surname == null ? other$surname != null : !this$surname.equals(other$surname)) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        final Object this$address = this.getAddress();
+        final Object other$address = other.getAddress();
+        if (this$address == null ? other$address != null : !this$address.equals(other$address)) return false;
+        final Object this$title = this.getTitle();
+        final Object other$title = other.getTitle();
+        if (this$title == null ? other$title != null : !this$title.equals(other$title)) return false;
+        final Object this$salary = this.getSalary();
+        final Object other$salary = other.getSalary();
+        if (this$salary == null ? other$salary != null : !this$salary.equals(other$salary)) return false;
+        final Object this$role = this.getRole();
+        final Object other$role = other.getRole();
+        if (this$role == null ? other$role != null : !this$role.equals(other$role)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Employee;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $surname = this.getSurname();
+        result = result * PRIME + ($surname == null ? 43 : $surname.hashCode());
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final Object $address = this.getAddress();
+        result = result * PRIME + ($address == null ? 43 : $address.hashCode());
+        final Object $title = this.getTitle();
+        result = result * PRIME + ($title == null ? 43 : $title.hashCode());
+        final Object $salary = this.getSalary();
+        result = result * PRIME + ($salary == null ? 43 : $salary.hashCode());
+        final Object $role = this.getRole();
+        result = result * PRIME + ($role == null ? 43 : $role.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Employee(surname=" + this.getSurname() + ", name=" + this.getName() + ", address=" + this.getAddress() + ", title=" + this.getTitle() + ", salary=" + this.getSalary() + ", role=" + this.getRole() + ")";
+    }
 }
